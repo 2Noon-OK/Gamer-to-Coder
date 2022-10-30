31df64284dcc5fd63bd4f9d41a84a0a55e436c4f
tongla1907 <tongla1907@gmail.com>
2022-10-30 11:02:51 +0700

    main file

---
+++ website/Pepsi-Card-UI-Design-main/app.js
@@ -0,0 +1,8 @@
+const scrollContainer = document.querySelector("main");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+
